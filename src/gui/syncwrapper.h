bf2644483d2a9cbdd0d2c52dd6544e480d0a74d0
Camila San <hello@camila.codes>
2019-11-25 22:15:09 +0100

    Use propfind to retrieve single file information.
    Signed-off-by: Camila San <hello@camila.codes>

--- src/gui/syncwrapper.h
+++ src/gui/syncwrapper.h
@@ -8,6 +8,9 @@
 #include "folderman.h"
 #include "csync.h"
 
+#include <QMap>
+#include "syncfileitem.h"
+
 namespace OCC {
 
 class SyncWrapper : public QObject
@@ -42,7 +45,7 @@ private:
     QString getRelativePath(QString path);
     bool shouldSync(const QString path);
     void sync(const QString path, bool is_fuse_created_file, csync_instructions_e instruction = CSYNC_INSTRUCTION_NEW);
-
+	QMap<QString, SyncFileItemPtr> _syncItemMap;
     //QMap<QString, bool> _syncDone;
 };
 }
